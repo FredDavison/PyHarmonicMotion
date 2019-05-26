93489e32d7c5f49af50bdbeff0fb2036a4217f47
FredDavison <fred.dev99@gmail.com>
2019-05-26 19:24:26 +0100

    Remove build folder

--- build/lib/pymech/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from configparser import ConfigParser
-
-
-import ipdb; ipdb.set_trace()
-assert os.path.exists('settings.conf')
-global_settings = ConfigParser()
-global_settings.read('settings.conf')
